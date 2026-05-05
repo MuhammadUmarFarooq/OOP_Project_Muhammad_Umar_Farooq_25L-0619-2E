7db49f26c1b21291e1d7f2c840c329cfdf6b30d7
Muhammad Umar Farooq <l250619@lhr.nu.edu.pk>
2026-05-05 05:20:03 +0500

    Some more files have been added.

---
+++ source/CoreCourse.h
@@ -0,0 +1,22 @@
+#ifndef CORECOURSE_H
+#define CORECOURSE_H
+
+class CoreCourse
+{
+private:
+    /* data */
+public:
+    CoreCourse(/* args */);
+    ~CoreCourse();
+};
+
+CoreCourse::CoreCourse(/* args */)
+{
+}
+
+CoreCourse::~CoreCourse()
+{
+}
+    
+
+#endif // CORECOURSE_H
